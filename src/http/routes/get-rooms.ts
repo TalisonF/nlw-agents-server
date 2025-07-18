1f1bb504dfdde7353c5ab4d06793e88c86aadf35
talisonf <talison737@gmail.com>
2025-07-17 21:21:37 -0300

    fix: room order list

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,4 +1,4 @@
-import { count, eq } from 'drizzle-orm';
+import { count, desc, eq } from 'drizzle-orm';
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
 import { db } from '../../db/connection.ts';
 import { schema } from '../../db/schema/index.ts';
@@ -18,7 +18,7 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       .where(eq(schema.rooms.userId, userId))
       .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
       .groupBy(schema.rooms.id)
-      .orderBy(schema.rooms.createdAt);
+      .orderBy(desc(schema.rooms.createdAt));
 
     return results;
   });
